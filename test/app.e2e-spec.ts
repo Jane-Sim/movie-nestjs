c6929fcc446185a033087c3f0bd0aa9bd04d4584
seyoung.sim <ssiox3@gmail.com>
2021-01-17 20:16:00 +0900

    e2e(end to end) 테스트 시작하기

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,29 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  describe('/movies', () => {
+    //get으로 movies 데이터를 불러올 때, 상태값 200과 빈배열을 받아오는지 테스트해보자.
+    // get API가 정상적으로 작동하는지 확인해보기
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+    //post로 데이터를 보낼 땐, send함수로 데이터를 먼저 보내고, 그 다음 201 상태값을 받아야한다.
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+    // 없는 데이터를 삭제할 때, Not Found인 404 상태값이 나오는지 확인하자.
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
 });
